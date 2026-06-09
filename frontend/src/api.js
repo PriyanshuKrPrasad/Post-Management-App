8f04a2e59c4c9a732ad84b26fa12837b9c6c37e9
Himanshu Prasad <priyanshukp224@gmail.com>
2026-06-09 21:56:56 +0530

    Updated production API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://post-management-app-i9vh.onrender.com/",
 });
 
 export default API;
